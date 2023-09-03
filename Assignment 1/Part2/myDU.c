b4bf33a08ea05b16cfce36249a4b6922533f84a1
Yash Gupta <yashbg@iitk.ac.in>
2023-09-04 05:17:54 +0530

    Restructure code

--- Assignment 1/Part2/myDU.c
+++ Assignment 1/Part2/myDU.c
@@ -6,111 +6,93 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-unsigned long getDirSize(const char *root) {
+unsigned long getSize(const char *root) {
 	struct stat rootStat;
 	if (stat(root, &rootStat) < 0) {
 		printf("Unable to execute\n");
 		exit(-1);
 	}
-	unsigned long size = rootStat.st_size;
 
-	DIR *d = opendir(root);
-	if (!d) {
-		printf("Unable to execute");
-		exit(-1);
+	if (S_ISREG(rootStat.st_mode)) {
+		// root is a file
+		return rootStat.st_size;
 	}
 
-	struct dirent *dir;
-	while (dir = readdir(d)) {
-		if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, "..")) {
-			continue;
+	if (S_ISDIR(rootStat.st_mode)) {
+		// root is a directory
+		unsigned long size = rootStat.st_size;
+
+		DIR *d = opendir(root);
+		if (!d) {
+			printf("Unable to execute");
+			exit(-1);
 		}
 
-		char dirPath[4097];
-		strcpy(dirPath, root);
-		strcat(dirPath, "/");
-		strcat(dirPath, dir->d_name);
-
-		if (dir->d_type == DT_REG) {
-			// dir is a file
-			struct stat dirStat;
-			if (stat(dirPath, &dirStat) < 0) {
-				printf("Unable to execute\n");
-				exit(-1);
-			}
-			
-			size += dirStat.st_size;
-			continue;
-		}
-
-		if (dir->d_type == DT_DIR) {
-			// dir is a directory
-			int pipefd[2];
-			if (pipe(pipefd) < 0) {
-				printf("Unable to execute\n");
-				exit(-1);
+		struct dirent *dir;
+		while (dir = readdir(d)) {
+			if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, "..")) {
+				continue;
 			}
 
-			pid_t pid = fork();
-			if (pid < 0) {
-				printf("Unable to execute\n");
-				exit(-1);
+			char dirPath[4097];
+			strcpy(dirPath, root);
+			strcat(dirPath, "/");
+			strcat(dirPath, dir->d_name);
+
+			if (dir->d_type == DT_REG || dir->d_type == DT_LNK) {
+				// dir is a file
+				size += getSize(dirPath);
+				continue;
 			}
 
-			if (!pid) {
-				// child
-				unsigned long size = getDirSize(dirPath);
-				
-				char sizeStr[21];
-				sprintf(sizeStr, "%lu", size);
-				if (write(pipefd[1], sizeStr, 21) != 21) {
+			if (dir->d_type == DT_DIR) {
+				// dir is a directory
+				int pipefd[2];
+				if (pipe(pipefd) < 0) {
 					printf("Unable to execute\n");
 					exit(-1);
 				}
 
-				exit(0);
-			}
+				pid_t pid = fork();
+				if (pid < 0) {
+					printf("Unable to execute\n");
+					exit(-1);
+				}
 
-			char sizeStr[21];
-			if (read(pipefd[0], sizeStr, 21) != 21) {
-				printf("Unable to execute\n");
-				exit(-1);
-			}
+				if (!pid) {
+					// child
+					unsigned long size = getSize(dirPath);
+					
+					char sizeStr[21];
+					sprintf(sizeStr, "%lu", size);
+					if (write(pipefd[1], sizeStr, 21) != 21) {
+						printf("Unable to execute\n");
+						exit(-1);
+					}
 
-			size += atol(sizeStr);
-			continue;
+					exit(0);
+				}
+
+				char sizeStr[21];
+				if (read(pipefd[0], sizeStr, 21) != 21) {
+					printf("Unable to execute\n");
+					exit(-1);
+				}
+
+				size += atol(sizeStr);
+				continue;
+			}
 		}
 
-		if (dir->d_type == DT_LNK) {
-			// dir is a symbolic link
-			struct stat dirStat;
-			if (stat(dirPath, &dirStat) < 0) {
-				printf("Unable to execute\n");
-				exit(-1);
-			}
-
-			if (S_ISREG(dirStat.st_mode)) {
-				// dir points to a file
-				size += dirStat.st_size;
-			}
-
-			else if (S_ISDIR(dirStat.st_mode)) {
-				// dir points to a directory
-				size += getDirSize(dirPath);
-			}
-			
-			continue;
-		}
+		closedir(d);
+		return size;
 	}
-
-	closedir(d);
-	return size;
 }
 
 int main(int argc, char *argv[]) {
 	char *root = argv[1];
 
-	unsigned long size = getDirSize(root);
+	unsigned long size = getSize(root);
 	printf("%lu\n", size);
 
 	return 0;
