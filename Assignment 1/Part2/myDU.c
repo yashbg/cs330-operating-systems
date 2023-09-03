7e56159b2597522eb5633a83a3b358d50818d103
Yash Gupta <yashbg@iitk.ac.in>
2023-09-04 02:25:46 +0530

    Use fork()

--- Assignment 1/Part2/myDU.c
+++ Assignment 1/Part2/myDU.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <string.h>
 
 unsigned long getDirSize(const char *root) {
 	DIR *d = opendir(root);
@@ -22,11 +24,45 @@ unsigned long getDirSize(const char *root) {
 		strcat(dirPath, "/");
 		strcat(dirPath, dir->d_name);
 
-		if (dir->d_type == DT_REG) {
-			// dir is a file
-			struct stat dirStat;
-			stat(dirPath, &dirStat);
-			size += dirStat.st_size;
+		struct stat dirStat;
+		stat(dirPath, &dirStat);
+		size += dirStat.st_size;
+
+		if (dir->d_type == DT_DIR) {
+			// dir is a directory
+			int pipefd[2];
+			if (pipe(pipefd) < 0) {
+				printf("Unable to execute\n");
+				exit(-1);
+			}
+
+			pid_t pid = fork();
+			if (pid < 0) {
+				printf("Unable to execute\n");
+				exit(-1);
+			}
+
+			if (!pid) {
+				// child
+				unsigned long size = getDirSize(dirPath);
+				
+				char sizeStr[21];
+				sprintf(sizeStr, "%lu", size);
+				if (write(pipefd[1], sizeStr, 21) != 21) {
+					printf("Unable to execute\n");
+					exit(-1);
+				}
+
+				exit(0);
+			}
+
+			char sizeStr[21];
+			if (read(pipefd[0], sizeStr, 21) != 21) {
+				printf("Unable to execute\n");
+				exit(-1);
+			}
+
+			size += atol(sizeStr);
 		}
 	}
 
