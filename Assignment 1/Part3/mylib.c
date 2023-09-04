41f5b682e71dafb9bda482f69abcfa1f83da3b01
Yash Gupta <yashbg@iitk.ac.in>
2023-09-05 00:43:01 +0530

    Request memory from OS

--- Assignment 1/Part3/mylib.c
+++ Assignment 1/Part3/mylib.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include "mylib.h"
 
 struct Node {
 	unsigned long size;
@@ -6,10 +8,29 @@ struct Node {
 	struct Node *prev;
 };
 
+void pushNode(struct Node *nodePtr);
+
 struct Node *freeHead = NULL;
 
 void *memalloc(unsigned long size) {
 	printf("memalloc() called\n");
+	if (!size) {
+		return NULL;
+	}
+
+	if (!freeHead) {
+		// request memory from OS
+		unsigned long mmapSize = size;
+		if (size % MMAP_MIN_SZ) {
+			mmapSize = ((size / MMAP_MIN_SZ) + 1) * MMAP_MIN_SZ;
+		}
+
+		void *memPtr = mmap(NULL, mmapSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+		if (memPtr == MAP_FAILED) {
+			return NULL;
+		}
+	}
+
 	return NULL;
 }
 
--- Assignment 1/Part3/mylib.h
+++ Assignment 1/Part3/mylib.h
@@ -1,6 +1,8 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 
+#define MMAP_MIN_SZ 4 * 1024 * 1024
+
 void *memalloc(unsigned long size);
 int memfree(void *ptr);
 
